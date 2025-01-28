1cda84211a10184c96de20760901e11414a2bd40
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2025-01-28 02:52:51 +0100

    redirecte réussi lorsque le client a un compte

--- e_commerce/commerce/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ e_commerce/commerce/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Models\Articles;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,10 +26,34 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        // $request->authenticate();
+
+        // $request->session()->regenerate();
+
+        // return redirect()->intended(RouteServiceProvider::HOME);
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            return back()->withErrors([
+                'email' => "Les informations d’identification fournies sont incorrectes.",
+            ]);
+        }
 
         $request->session()->regenerate();
 
+        // Vérifie si l'utilisateur connecté a des articles
+        $user = Auth::user();
+        $hasArticles = Articles::where('users_id', $user->id)->exists();
+
+        // Redirection conditionnelle
+        if ($hasArticles) {
+            return redirect('/articles_publier'); // URL vers la page des articles
+        }
+
+        // Sinon, redirige vers la page par défaut
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
